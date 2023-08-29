7842d27a28da9f14f4cc4f2dae73a34a5bc8f7ab
Laboratório 02 - UMFG <umfg.lab02@faculdadefacec.intranet>
2023-08-28 21:13:24 -0300

    DIA-28-08-2023/V1

---
+++ src/PessoaFisica.java
@@ -0,0 +1,9 @@
+public class PessoaFisica {
+    private String nome;
+    private String cpf;
+
+    public PessoaFisica(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+}
